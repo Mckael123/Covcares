6e5da8a265231f061a8fed298420309f7cd3e197
McKael-forti <76393985+Mckael123@users.noreply.github.com>
2021-05-20 23:11:18 +0700

    Add files via upload

---
+++ laravel-project/resource/news.blade.php
@@ -0,0 +1,153 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>news</title>
+    <link rel="stylesheet" type="text/css" href="css/news.css">
+    <link rel="shortcut icon" href="/image/HomePage LOGO.png" type="image/x-icon">
+</head>
+
+<body>
+    <div class="container">
+        <section class="header">
+            <div class="logo"><a href="{{url('/')}}"><img src="/image/HomePage LOGO.png"></a></div>
+            <a href="{{url('/')}}" class="inactive">Home</a>
+            <a href="{{url('/landproduk')}}" class="inactive">See Covid Kits</a>
+            <a href="{{url('/news')}}" class="inactive">News</a>
+            <a href="{{url('/about')}}" class="inactive">About Us</a>
+            <a href="{{url('/covinfo')}}" class="inactive">What Is COVID-19?</a>
+            <button class="login"> <a href="{{url('/login')}}">
+                    <div class="logintxt">Login</div>
+                </a></button>
+        </section>
+
+        <section class="content">
+            <div class="title">
+                <h1>News</h1>
+                <div class="underline"></div>
+            </div>
+            <div class="isi-content">
+                <div class="isi-content-atas">
+                    <div class="update">
+                        <h2>Update Article</h2>
+                        <a href="berita.html"><img src="/image/update.png" alt=""></a>
+                    </div>
+                    <div class="latest">
+                        <h2>Latest Articles</h2>
+                        <div class="latest-isi">
+                            <img src="/image/isi1.png" alt="">
+                            <p>Vaccinations Postponed,This Is News Of The Bandung Coronavirus Clinical Trial</p>
+                        </div>
+                        <div class="latest-isi"><img src="/image/isi2.png" alt="">
+                            <p>Corona Virus Predicted to be
+                                Endemic,Is that Right?</p>
+                        </div>
+                        <div class="latest-isi"><img src="/image/isi3.png" alt="">
+                            <p>Vaccinations Postponed,
+                                This Is News Of The Bandung
+                                Coronavirus Clinical Trial</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="isi-content-bawah">
+                    <h2>Most Popular Articles About Health</h2>
+                    <div class="popular-area">
+                        <div class="row-popular">
+                            <div class="popular-box">
+                                <img src="/image/content1.png" alt="">
+                                <h3>
+                                    Corona vaccine is still needed
+                                    even though it has been infected</h3>
+                                <p>Do those who have been infected
+                                    with COVID-19 still need
+                                    two doses of the corona vaccine?
+                                    Here is the review!</p>
+
+                            </div>
+                            <div class="popular-box">
+                                <img src="/image/content2.png" alt="">
+                                <h3>Flood Season, Can Corona
+                                    Transmitted Through Water?</h3>
+                                <p>Floods are occurring simultaneously
+                                    with the unfinished coronavirus pandemic.
+                                    Concerns arose as to whether the corona</p>
+                            </div>
+                            <div class="popular-box">
+                                <img src="/image/content3.png" alt="">
+                                <h3>COVID-19 Vaccination for the Elderly,
+                                    Pay Attention to This</h3>
+                                <p>What needs to be considered by the elderly
+                                    who will get the COVID-19 vaccine?
+                                    Check out the full review</p>
+                            </div>
+                            <div class="popular-box">
+                                <img src="/image/content4.png" alt="">
+                                <h3>ASD and VSD Congenital
+                                    Heart Disease in Children</h3>
+                                <p>ASD and VSD are common congenital heart
+                                    diseases experienced by newborns.
+                                    To prevent it</p>
+                            </div>
+                        </div>
+                        <div class="row-popular">
+                            <div class="popular-box"><img src="/image/content5.png" alt="">
+                                <h3>The Right Age to Start Child
+                                    Sex Education</h3>
+                                <p>Don't hesitate to start giving sex
+                                    education to your little one.
+                                    This is the right age to start </p>
+                            </div>
+                            <div class="popular-box"><img src="/image/content6.png" alt="">
+                                <h3>Beware of People with Diabetes
+                                    Vulnerable to This</h3>
+                                <p>Eye pain due to diabetes is triggered
+                                    by high insulin levels,
+                                    so that a number
+                                    of eye disease symptoms appear.</p>
+                            </div>
+                            <div class="popular-box"><img src="/image/content7.png" alt="">
+                                <h3>swollen skin after being bitten
+                                    by a cat, what to do?</h3>
+                                <p>Cat bites can cause swelling,
+                                    injury, and infection.
+                                    The following actions
+                                    must be taken immediately </p>
+                            </div>
+                            <div class="popular-box"><img src="/image/content8.png" alt="">
+                                <h3>Here's How to Take Care
+                                    of a Newborn Kitten</h3>
+                                <p>Taking care of newborn kittens
+                                    is a difficult and easy thing to do.
+                                    But don't worry,</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="footer">
+            <div class="connect">
+                <label class="conus">Connect with Us</label><br>
+                <a href="" class="iconconnect"><img src="/image/instagram.png" alt=""></a>
+                <a href="" class="iconconnect"><img src="/image/Facebook.png" alt=""></a>
+            </div>
+            <div class="contact">
+                <label class="contus">Contact us:</label><br>
+                <div class="baris">
+                    <img src="/image/wa.png" alt=""><label class="rightfooter">+62 822-1780-2911</label>
+                </div>
+                <div class="baris">
+                    <img src="/image/email.png" alt=""><label class="rightfooter">covcare@gmail.com</label>
+                </div>
+
+
+            </div>
+        </section>
+    </div>
+
+</body>
