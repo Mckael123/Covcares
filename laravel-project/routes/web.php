a4b6a28521813238b8eba0ef50c974930f0a8b28
gitasafitri14 <84491236+gitasafitri14@users.noreply.github.com>
2021-05-21 01:07:10 +0700

    Update web.php

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,18 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/landproduk', function () {
-    return view('landproduk');
-});
-Route::get('/news', function () {
-    return view('news');
-});
-Route::get('/covinfo', function () {
-    return view('covinfo');
-});
-Route::get('/login', function () {
-    return view('login');
-});
+
+
+Route::get('/',[PagesController::class,'home']);
+Route::get('/indexlogin',[PagesController::class,'indexlogin']);
+Route::get('/about',[PagesController::class,'about']);
+Route::get('/about2',[PagesController::class,'about2']);
+Route::get('/admin',[PagesController::class,'admin']);
+Route::get('/login',[PagesController::class,'login']);
+Route::get('/registrasi',[PagesController::class,'registrasi']);
+Route::get('/editnews',[PagesController::class,'editnews']);
+Route::get('/verifylist',[PagesController::class,'verifylist']);
+Route::get('/verifyadmin',[PagesController::class,'verifyadmin']);
+Route::get('/editproduct',[PagesController::class,'editproduct']);
